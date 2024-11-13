d3ffe7044a4914bc5ebb98e20c95fc99a0c3e021
Marina Tarasenko <zvezda_marina@mail.ru>
2024-11-13 08:26:06 +0300

    test second submition

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,12 @@ function images() {
     .pipe(browserSync.reload({ stream: true }));
 }
 
+function fonts() {
+  return gulp.src('src/fonts/*.{woff,woff2}', { encoding: false })
+    .pipe(gulp.dest('dist/fonts/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
 function scripts() {
   return gulp.src('src/scripts/*.js')
     .pipe(gulp.dest('dist/scripts/'))
@@ -53,15 +59,17 @@ function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/blocks/**/*.css'], css);
   gulp.watch(['src/images/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/*.{woff,woff2}'], fonts);
   gulp.watch(['src/scripts/*.js'], scripts);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images, scripts));
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, scripts));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
+exports.fonts = fonts;
 exports.scripts = scripts;
 exports.clean = clean;
 
